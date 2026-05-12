539b7775c028116396982e5e6ab561fae1919296
Marlon136 <m.londonot@uniandes.edu.co>
2026-05-11 22:15:59 -0500

    preparcial

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,  // descarta campos no declarados en el DTO
-      transform: true,  // convierte string→number en @Query()
+      whitelist: true, // descarta campos no declarados en el DTO
+      transform: true, // convierte string→number en @Query()
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
